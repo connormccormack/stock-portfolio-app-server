91d044281f002e5a31fb607f78a2c72373928e01
Connor McCormack <mccormack.connor.m@gmail.com>
2020-04-24 15:27:25 -0500

    deploying db

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-const { PORT, NODE_ENV, DB_URL, TEST_DB_URL, API_TOKEN, JWT_SECRET, CLIENT_ORIGIN } = process.env;
+const { PORT, NODE_ENV, DATABASE_URL, TEST_DATABASE_URL, API_TOKEN, JWT_SECRET, CLIENT_ORIGIN } = process.env;
 
 module.exports = {
   PORT: PORT || 8000,
   NODE_ENV: NODE_ENV || 'development',
-  DB_URL: DB_URL || 'postgresql://postgres@localhost/stock_portfolio_database',
-  TEST_DB_URL: TEST_DB_URL || 'postgresql://postgres@localhost/stock_portfolio_database_test',
+  DATABASE_URL: DATABASE_URL || 'postgresql://postgres@localhost/stock_portfolio_database',
+  TEST_DATABASE_URL: TEST_DATABASE_URL || 'postgresql://postgres@localhost/stock_portfolio_database_test',
   API_TOKEN,
   JWT_SECRET,
   CLIENT_ORIGIN
